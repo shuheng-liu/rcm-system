d11be7171e6ea40251726058e11c8ed155cea5d8
Shuheng Liu <wish1104@icloud.com>
2021-01-10 20:46:33 +0800

    test(model): add test case for model

--- tests/test_models.py
+++ tests/test_models.py
@@ -82,3 +82,48 @@ def test_staff():
     pass
 
     clean_up()
+
+
+def test_course():
+    from models import Course, Instructor, Staff, Student
+    from datetime import date
+    today = date.today()
+
+    # instructors
+    joe = Instructor(first_name='Joe', last_name='Biden', email='joe@biden.com', password='pwd').save()
+    tony = Instructor(first_name='Tony', last_name='Stark', email='tony@stark.com', password='pwd').save()
+    kamala = Instructor(first_name='Kamala', last_name='Harris', email='kamala@harris.com', password='pwd').save()
+    pepper = Instructor(first_name='Pepper', last_name='Potts', email='pepper@potts.com', password='pwd').save()
+    morgan = Instructor(first_name='Morgan', last_name='Stark', email='morgan@stark.com', password='pwd').save()
+
+    # coordinators
+    james = Staff(first_name='James', last_name='Bond', email='james@bond.com', password='pwd').save()
+    eve = Staff(first_name='Eve', last_name='Moneypenny', email='eve@moneypenny.com', password='pwd').save()
+
+    # students
+    john = Student(first_name='John', last_name='Doe', email='john@doe.com', password='pwd', gender='M').save()
+    jane = Student(first_name='Jane', last_name='Doe', email='jane@doe.com', password='pwd', gender='F').save()
+
+    # w/o start_date
+    Course(code='PL999', course_name='US Presidency', professor=joe).save()
+    # w/ start_date
+    Course(code='PL888', start_date=today, course_name='US Senatorship', professor=joe).save()
+    # w/o mentors, coordinator, and students
+    Course(code='PL101', course_name='Introduction to Politics', professor=joe).save()
+    # w/ single mentor; w/o coordinator and students
+    Course(code='PL102', course_name='Intermediate Politics', professor=joe, mentors=[kamala]).save()
+    # w/ mentors and coordinator; w/o students
+    Course(code='CS101', course_name='Introduction to Computer Science',
+           professor=tony, mentors=[pepper, morgan], coordinator=james).save()
+    # w/ mentors, coordinator, and students
+    Course(code='CS103', course_name='Advanced Computer Science',
+           professor=tony, mentors=[pepper, morgan], coordinator=eve, students=[john, jane]).save()
+
+    # w/o professor
+    with pytest.raises(ValidationError):
+        Course(code='PL103', course_name='Advanced Politics').save()
+    # duplicate course code
+    with pytest.raises(NotUniqueError):
+        Course(code='CS103', course_name='Computational Science', professor=tony).save()
+
+    clean_up()
